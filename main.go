92b7846d9b12930a47a841afcc790af0a874168a
eduardohass <eduardohass@outlook.com>
2024-04-21 18:18:26 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/eduardohass/dica/router"
+import (
+	"github.com/eduardohass/dica/config"
+	"github.com/eduardohass/dica/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	// Initialize router
 	router.Initialize()
 }
