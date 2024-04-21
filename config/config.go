215313c93a1e7be0f7b1a6264ffe6c0056e27fdb
eduardohass <eduardohass@outlook.com>
2024-04-21 18:52:13 -0300

    feat: add database handler

--- config/config.go
+++ config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+
 	"gorm.io/gorm"
 )
 
@@ -10,8 +12,20 @@ var (
 )
 
 func Init() error {
+	var err error
+
+	// Initialize SQLite
+	db, err = InitializeSQLite()
+
+	if err != nil {
+		return fmt.Errorf("Error initilizing sqlite %v", err)
+	}
 	// return errors.New("fake error")
 	return nil
+}
+
+func GetSQLite() *gorm.DB {
+	return db
 }
 
 func GetLogger(p string) *Logger {
