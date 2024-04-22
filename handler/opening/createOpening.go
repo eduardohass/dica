3be302f21c3c1828b00ed11e280a418d7e8735f8
eduardohass <eduardohass@outlook.com>
2024-04-22 19:05:52 -0300

    feat: organizing handlers

--- handler/opening/createOpening.go
+++ handler/opening/createOpening.go
@@ -8,17 +8,17 @@ import (
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
-	request := CreateOpenningRequest{}
+	request := CreateOpeningRequest{}
 
 	ctx.BindJSON(&request)
 
 	if err := request.Validate(); err != nil {
 		logger.Errorf("validation error: %v", err.Error())
-		SendError(ctx, http.StatusBadRequest, err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
 		return
 	}
 
-	opening := schemas.Openning{
+	opening := schemas.Opening{
 		Role:     request.Role,
 		Company:  request.Company,
 		Location: request.Location,
@@ -29,7 +29,7 @@ func CreateOpeningHandler(ctx *gin.Context) {
 
 	if err := db.Create(&opening).Error; err != nil {
 		logger.Errorf("error creating opening: %v", err.Error())
-		SendError(ctx, http.StatusInternalServerError, "error creating opening on database")
+		sendError(ctx, http.StatusInternalServerError, "error creating opening on database")
 		return
 	}
 
