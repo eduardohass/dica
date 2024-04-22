7982cee88733b2b8704949d5d73eaef920b6d323
eduardohass <eduardohass@outlook.com>
2024-04-22 19:04:51 -0300

    feat: add list opening handler logic

---
+++ handler/opening/listOpenings.go
@@ -0,0 +1,17 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/eduardohass/dica/schemas"
+	"github.com/gin-gonic/gin"
+)
+
+func ListOpeningsHandler(ctx *gin.Context) {
+	openings := []schemas.Opening{}
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+	sendSuccess(ctx, "list-openings", openings)
+}
