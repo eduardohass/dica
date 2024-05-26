71898caec5f529ac99b126d80c6a3ac1295313d4
Eduardo Hass <eduardohass@outlook.com>
2024-05-26 13:51:28 -0300

    feat: pg conn string to variables

--- config/postgres.go
+++ config/postgres.go
@@ -4,12 +4,21 @@ import (
 	"github.com/eduardohass/dica/schemas"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
+	"fmt"
 )
 
 func InitializePostgres() (*gorm.DB, error) {
 	logger := GetLogger("postgres")
-	dbURL := "postgres://postgres:changeme@postgres:5432/dica"
+	// dbURL := "postgres://postgres:changeme@postgres:5432/dica"
 	// dbURL := os.Getenv("CON_STR")
+	dbURL := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=America/Sao_Paulo",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
 
 	// Create DB and connect
 	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
