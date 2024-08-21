0e977e983f58c4282a8aa012c71f38b1a1144576
eduardohass <eduardohass@outlook.com>
2024-08-21 19:48:21 -0300

    feat: add handlers

--- router/routes.go
+++ router/routes.go
@@ -2,7 +2,11 @@ package router
 
 import (
 	docs "github.com/eduardohass/dica/docs"
+	handlerAnswer "github.com/eduardohass/dica/handler/answer"
+	handlerLetter "github.com/eduardohass/dica/handler/letter"
 	handlerOpening "github.com/eduardohass/dica/handler/opening"
+	handlerQuestion "github.com/eduardohass/dica/handler/question"
+	handlerQuestionAnswer "github.com/eduardohass/dica/handler/questionAnswer"
 
 	// handlerUserAnswer "github.com/eduardohass/dica/handler/userAnswer"
 	"github.com/gin-gonic/gin"
@@ -12,10 +16,10 @@ import (
 
 func initializeRoutes(router *gin.Engine) {
 	handlerOpening.InitializeHandler()
-	// handlerLetter.InitializeHandler()
-	// handlerAnswer.InitializeHandler()
-	// handlerQuestion.InitializeHandler()
-	// handlerQuestionAnswer.InitializeHandler()
+	handlerLetter.InitializeHandler()
+	handlerAnswer.InitializeHandler()
+	handlerQuestion.InitializeHandler()
+	handlerQuestionAnswer.InitializeHandler()
 	// handlerUserAnswer.InitializeHandler()
 	basePath := "/api/v1"
 	docs.SwaggerInfo.BasePath = basePath
@@ -28,30 +32,30 @@ func initializeRoutes(router *gin.Engine) {
 		v1.DELETE("opening", handlerOpening.DeleteOpeningHandler)
 		v1.GET("openings", handlerOpening.ListOpeningsHandler)
 		// Letter
-		// v1.GET("letter", handlerLetter.ShowLetterHandler)
-		// v1.POST("letter", handlerLetter.CreateLetterHandler)
-		// v1.PUT("letter", handlerLetter.UpdateLetterHandler)
-		// v1.DELETE("letter", handlerLetter.DeleteLetterHandler)
-		// v1.GET("letters", handlerLetter.ListLettersHandler)
+		v1.GET("letter", handlerLetter.ShowLetterHandler)
+		v1.POST("letter", handlerLetter.CreateLetterHandler)
+		v1.PUT("letter", handlerLetter.UpdateLetterHandler)
+		v1.DELETE("letter", handlerLetter.DeleteLetterHandler)
+		v1.GET("letters", handlerLetter.ListLettersHandler)
 		// // Question
-		// v1.GET("question", handlerQuestion.ShowQuestionHandler)
-		// v1.POST("question", handlerQuestion.CreateQuestionHandler)
-		// v1.PUT("question", handlerQuestion.UpdateQuestionHandler)
-		// v1.DELETE("question", handlerQuestion.DeleteQuestionHandler)
-		// v1.GET("question", handlerQuestion.ListQuestionsHandler)
+		v1.GET("question", handlerQuestion.ShowQuestionHandler)
+		v1.POST("question", handlerQuestion.CreateQuestionHandler)
+		v1.PUT("question", handlerQuestion.UpdateQuestionHandler)
+		v1.DELETE("question", handlerQuestion.DeleteQuestionHandler)
+		v1.GET("question", handlerQuestion.ListQuestionsHandler)
 		// QuestionAnswer
-		// v1.GET("questionAnswer", handlerQuestionAnswer.ShowQuestionAnswerHandler)
-		// v1.POST("questionAnswer", handlerQuestionAnswer.CreateQuestionAnswerHandler)
-		// v1.PUT("questionAnswer", handlerQuestionAnswer.UpdateQuestionAnswerHandler)
-		// v1.DELETE("questionAnswer", handlerQuestionAnswer.DeleteQuestionAnswerHandler)
-		// v1.GET("questionAnswer", handlerQuestionAnswer.ListQuestionsAnswerHandler)
+		v1.GET("questionAnswer", handlerQuestionAnswer.ShowQuestionAnswerHandler)
+		v1.POST("questionAnswer", handlerQuestionAnswer.CreateQuestionAnswerHandler)
+		v1.PUT("questionAnswer", handlerQuestionAnswer.UpdateQuestionAnswerHandler)
+		v1.DELETE("questionAnswer", handlerQuestionAnswer.DeleteQuestionAnswerHandler)
+		v1.GET("questionAnswer", handlerQuestionAnswer.ListQuestionsAnswerHandler)
 		// // Answer
-		// v1.GET("answer", handlerAnswer.ShowAnswerHandler)
-		// v1.POST("answer", handlerAnswer.CreateAnswerHandler)
-		// v1.PUT("answer", handlerAnswer.UpdateAnswerHandler)
-		// v1.DELETE("answer", handlerAnswer.DeleteAnswerHandler)
-		// v1.GET("answer", handlerAnswer.ListAnswersHandler)
-		// UserAnswer
+		v1.GET("answer", handlerAnswer.ShowAnswerHandler)
+		v1.POST("answer", handlerAnswer.CreateAnswerHandler)
+		v1.PUT("answer", handlerAnswer.UpdateAnswerHandler)
+		v1.DELETE("answer", handlerAnswer.DeleteAnswerHandler)
+		v1.GET("answer", handlerAnswer.ListAnswersHandler)
+		// // UserAnswer
 		// v1.GET("userAnswer", handlerUserAnswer.ShowUserAnswerHandler)
 		// v1.POST("userAnswer", handlerUserAnswer.CreateUserAnswerHandler)
 		// v1.PUT("userAnswer", handlerUserAnswer.UpdateUserAnswerHandler)
